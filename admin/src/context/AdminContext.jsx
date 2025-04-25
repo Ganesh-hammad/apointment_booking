733023f6071bfb983223dfcb8a8e42c7e1c2f5be
Ganesh-hammad <144488855+Ganesh-hammad@users.noreply.github.com>
2025-04-25 17:17:27 +0530

    Update AdminContext.jsx

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -9,7 +9,7 @@ const AdminContextProvider = (props) =>{
     const navigate = useNavigate();
 
     const [aToken, setAToken] = useState(localStorage.getItem('aToken') ? localStorage.getItem('aToken'):'');
-    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const backendUrl = "https://apointment-booking-backend.onrender.com";
     const [doctors, setDoctors] = useState([])
     const [appointments, setAppointments] = useState([]);
     const [dashBoardData, setDashBoardData] = useState(false);
